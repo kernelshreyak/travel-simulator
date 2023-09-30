b18e459dee804da32e327204299045713e078a29
Shreyak Chakraborty <shreyak.rekshda@gmail.com>
2023-09-30 13:28:12 +0530

    added build script, heaversine

--- backend/converter.js
+++ backend/converter.js
@@ -6,31 +6,6 @@ const fs = require("fs");
 csv()
 .fromFile("./stations.csv")
 .then((jsonObj)=>{
-    // console.log(jsonObj);
     fs.writeFileSync("./stations.json",JSON.stringify(jsonObj));
     console.log("Completed")
-})
-
-
-// convert GeoJSON to custom CSV
-
-// const stationsData = require("./stations");
-// // console.log(stationsData);
-
-// const csvRows = [];
-// let writeStream = fs.createWriteStream('./stations.csv');
-// writeStream.write(["name","id","longitude_deg","latitude_deg"].join(',')+ '\n');
-// stationsData.stationsData.features.forEach(station => {
-//     // if(station.properties.name) console.log(station.properties.name);
-//     if(station.geometry){
-//         writeStream.write([station.properties.name ?? "Station",station.properties.id ?? "NOID",parseFloat(station.geometry.coordinates[0]).toFixed(7),
-//         parseFloat(station.geometry.coordinates[1]).toFixed(7)].join(',')+ '\n');
-//     }
-    
-// });
-// writeStream.end();
-// writeStream.on('finish', () => {
-//     console.log('CSV created')
-// }).on('error', (err) => {
-//     console.log(err)
-// })
+});
