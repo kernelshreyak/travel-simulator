75b84b222dda533d7f2f1fc966e2b1fcead0b43b
Shreyak Chakraborty <shreyak.rekshda@gmail.com>
2023-09-30 15:29:49 +0530

    updated new API on render.com

--- src/config.js
+++ src/config.js
@@ -1,15 +1,19 @@
 export const config = {
-  // API_URL: "https://world-travel-simulator.herokuapp.com/api",
-  API_URL: "http://localhost:8000/api",
+  API_URL: "https://world-travelsim-backend.onrender.com/api",
+  // API_URL: "http://localhost:8000/api",
   AUTH0_DOMAIN: "dev-jpr60ht5.us.auth0.com",
   AUTH0_CLIENTID: "s0OzlM1MElFjdjtNgq8fwVBsD2Ko3jiG",
   APP_URL: "http://localhost:3000",
   // APP_URL: "https://world-travel-simulator.vercel.app",
   mapVehicleTypes: {
-    INTRACITY: {name: "Intra-City",speed: "Slow",costperkm: 10},
-    INTERCITY: {name: "Inter-City",speed: "Fast",costperkm: 15},
-    INTERSTATE: {name: "Inter State",speed: "Very Fast",costperkm: 40},
-    TRAIN: {name: "Train",speed: "Very Fast",costperkm: 15},
-    "INTERCOUNTY - AIRLINE": {name: "Inter-Country Airplane",speed: "Fastest",costperkm: 100},
-  }
-}
+    INTRACITY: { name: "Intra-City", speed: "Slow", costperkm: 10 },
+    INTERCITY: { name: "Inter-City", speed: "Fast", costperkm: 15 },
+    INTERSTATE: { name: "Inter State", speed: "Very Fast", costperkm: 40 },
+    TRAIN: { name: "Train", speed: "Very Fast", costperkm: 15 },
+    "INTERCOUNTY - AIRLINE": {
+      name: "Inter-Country Airplane",
+      speed: "Fastest",
+      costperkm: 100,
+    },
+  },
+};
